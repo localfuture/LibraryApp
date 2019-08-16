4feb27727e9abc019b987d46ce2d7ea4039ff0d3
Anand Alex <anandalex1993@gmail.com>
2019-08-16 14:48:02 +0530

    title and navbar complete

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var app = express();
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
 
+navbar =[{link:"link of the book1",title:"book title1"},
+{link:"link of the book2",title:"book title2"}];
+
 app.get("/",(req,res)=>{
-    res.render("index");
+    res.render("index",{navbar:navbar});
 });
 
 app.listen(3000,()=>{
